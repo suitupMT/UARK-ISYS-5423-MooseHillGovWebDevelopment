fee50c1bc03fed055c8356af15711a9f7f276509
Matthew Templeton <suitup.mt@gmail.com>
2021-10-11 16:26:10 -0500

    Added Main Landing Page-Covid Page- SignUp Page

---
+++ WebFileFramework/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+    <link href="./css/signUp.css" rel="stylesheet" />
+    <link href="./css/forms.css" rel="stylesheet" />
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="v168_4">
+        <div class="v168_28"></div>
+        <div class="v168_29"><span class="v168_30">Log Into Your Moose Profile</span></div>
+        <div class="v168_44"></div>
+        <section class="singup-form">
+
+            <form action="includes/login.inc.php" method="post">
+
+                <div class="v168_50">
+                    <input type="text" name="E-mail" placeholder="Email...">
+                </div>
+                <div class="v168_51">
+                    <input type="password" name="password" placeholder="Password...">
+                </div>
+
+                <div class="v168_58">
+                    <button type="submit" name="submit">Log In</button>
+                </div>
+            </form>
+
+        </section>
+
+
+
+    </div>
+</body>
+
+</html>
