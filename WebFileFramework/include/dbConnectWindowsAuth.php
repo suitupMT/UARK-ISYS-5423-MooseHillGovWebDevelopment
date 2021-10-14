c47083696a5403de530c74fedd0e0ab4edb60ce8
suitupMT <suitup.mt@gmail.com>
2021-10-13 20:36:18 -0500

    dbConnect won't display connection message

--- WebFileFramework/include/dbConnectWindowsAuth.php
+++ WebFileFramework/include/dbConnectWindowsAuth.php
@@ -7,9 +7,8 @@ $connectionInfo = array( "Database"=>"MooseHideLanding");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
 
 if( $conn ) {
-     echo "Connection established.<br />";
+     //echo "Connection established.<br />";
 }else{
      echo "Connection could not be established.<br />";
      die( print_r( sqlsrv_errors(), true));
 }
-?>
