20d07856804bc7b3fbe4a0a7b346619aa7a66bc7
suitupMT <suitup.mt@gmail.com>
2021-10-20 05:46:52 -0500

    fix logout message

--- WebFileFramework/include/logout.inc.php
+++ WebFileFramework/include/logout.inc.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
-    <link href="../css/RedirectMessages.css" rel="stylesheet" />
+    <link href="../css/logout.css" rel="stylesheet" />
     <title>Logout</title>
 
 </head>
@@ -12,7 +12,14 @@
 <?php
 session_start(); //to ensure you are using same session
 session_destroy(); //destroy the session
-echo '<div class="my-notify-success">Thank you for being a part of MooseLanding!</br>You are successfully logged out. Redirecting you to the Main City Page</div>';
+echo ('
+<div class="v174_4">
+                <span class="v211_5">Thanks for being part of the Moose!</span>
+                <span class="v211_6">You have successfully logged out. </span>
+                <span class="v211_7">You will be redirected to the City main page soon. </span>
+            </div>
+
+');
 //echo "Thank you for being a part of MooseLanding!</br>";
 //echo "You are successfully logged out. Redirecting you to the Main City Page</br>";
 header("refresh:4 url= ../main.php"); //to redirect back to "index.php" after logging out
