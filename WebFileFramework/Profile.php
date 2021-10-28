0eb3225d217a766cc327700baf8eb2315b47bece
suitupMT <suitup.mt@gmail.com>
2021-10-28 11:14:41 -0500

    profile image display

--- WebFileFramework/Profile.php
+++ WebFileFramework/Profile.php
@@ -1,2 +1,18 @@
 <?php
 session_Start();
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+    <link href="./css/profile.css" rel="stylesheet" />
+
+
+</head>
+
+<body>
+    <div class="vPage"> </div>
+</body>
+
+</html>
